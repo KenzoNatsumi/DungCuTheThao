407d9c7f83e7c89ac35a80e120d6ace7b4795669
Kenzo <khoa1237701@gmail.com>
2025-04-22 19:45:53 +0700

    sua server.js, sua edit.html

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(url);
 const dbName = 'dsSanPham';
 const collectionName = 'DungCuTheThao';
 
